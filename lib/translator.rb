e38c36aa1a8c6639284c911d989dfa2ea82792f2
david v perekopskiy <david.perekopskiy@go.shoreline.edu>
2020-01-20 02:21:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,8 +3,8 @@ require "yaml"
 def load_library(path)
   # code goes here
   emoticons= YAML.load_file(path)
-  hash={:get_meaning=>{},
-  :get_emoticon=>{}}
+  hash={"get_meaning"=>{},
+  "get_emoticon"=>{}}
   emoticons.each {|value,key|
   hash[:get_meaning][value[0]]=emoticons[key][1]
   hash[:get_emoticon][value[1]]=key}
